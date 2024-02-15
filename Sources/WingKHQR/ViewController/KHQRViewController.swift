19e979de98d9ba2aa20f6b54bec6c8692cf22fd2
kemvanra ouch <ouchkemvanra1@gmail.com>
2024-02-15 09:50:38 +0700

    public require init

--- Sources/WingKHQR/ViewController/KHQRViewController.swift
+++ Sources/WingKHQR/ViewController/KHQRViewController.swift
@@ -111,7 +111,7 @@ open class KHQRViewController: UIViewController{
     super.init(nibName: nil, bundle: nil)
   }
   
-  required init?(coder: NSCoder) {
+  public required init?(coder: NSCoder) {
     fatalError("init(coder:) has not been implemented")
   }
   
